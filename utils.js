9ac981e7a1853a6082c5b434c16a640dcb480ad9
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-08-14 21:12:56 +0100

    Don't keep asking

--- utils.js
+++ utils.js
@@ -2,8 +2,8 @@ suffix = (navigator.userAgent.includes("Mobile")) ? Date.now() : "";
 
 function getState(length) {
     state = (localStorage.hasOwnProperty("state")) ? parseInt(localStorage.getItem("state")) + 1: 0;
-    if(isNaN(state) || state > length) state = 0;
-    if (confirm('Allow state to be saved to localStorage?')) localStorage.setItem("state", state);
+    if (isNaN(state) || state > length) state = 0;
+    if (confirm('Allow state to be saved to localStorage?') || localStorage.getItem("state") !== null) localStorage.setItem("state", state);
 }
 
 async function toDataURL(url) {

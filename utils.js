bf6f8b0f7da997ca31a34de5b09512ea1157aca1
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-08-14 21:15:06 +0100

    Dont even use it

--- utils.js
+++ utils.js
@@ -1,11 +1,5 @@
 suffix = (navigator.userAgent.includes("Mobile")) ? Date.now() : "";
 
-function getState(length) {
-    state = (localStorage.hasOwnProperty("state")) ? parseInt(localStorage.getItem("state")) + 1: 0;
-    if (isNaN(state) || state > length) state = 0;
-    if (confirm('Allow state to be saved to localStorage?') || localStorage.getItem("state") !== null) localStorage.setItem("state", state);
-}
-
 async function toDataURL(url) {
     return fetch(url).then((response) => {
             return response.blob();
